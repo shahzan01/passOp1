b56054b89e87b44fd7d457e7cf460a63ff9df375
Mohd shahzan <m.shahzan2003@gmail.com>
2026-04-17 22:29:49 +0530

    testing

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
-const app = express;
+const app = express();
 
-app.use(express.json);
+app.use(express.json();
 
 app.get('/', (req, res) => {
   res.send({ message: 'Server is running' });
