021c01ad574d7491a467ecdbf417b5b70f5fc72b
Mohd shahzan <m.shahzan2003@gmail.com>
2026-04-17 22:30:32 +0530

    Fix syntax error in express.json() middleware

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send({ message: 'Server is running' });
